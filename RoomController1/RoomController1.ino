391c3a95b2c61dc236871f825831c81b69663c5d
Konrad Pawłowski <konrad.pawlowski@kps-system.pl>
2019-11-17 18:27:07 +0100

    roomCOntroller update

--- RoomController1/RoomController1.ino
+++ RoomController1/RoomController1.ino
@@ -30,43 +30,64 @@
 */
 
 // Enable debug prints to serial monitor
+#include <AsyncTaskLib.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+
+#define NODE_ID		32
+
 #define MY_DEBUG 
 
 // Enable and select radio type attached
 #define MY_RS485
-#define MY_RS485_DE_PIN 2
-//#define MY_RADIO_RFM69
+#define MY_RS485_DE_PIN 7
+#define MY_NODE_ID	NODE_ID
 #define MY_INCLUSION_MODE_FEATURE
 #define MY_RS485_BAUD_RATE 9600
-// Enabled repeater feature for this node
-//#define MY_REPEATER_FEATURE
 
+#define MS_BOARD_NAME               "PatrykRooms"
+#define MS_SOFTWARE_VERSION         "2.0"
+#define MS_RELAY1_CHILD_ID          1
+#define MS_RELAY2_CHILD_ID          2
+#define MS_RELAY3_CHILD_ID          3
+#define MS_TEMP_CHILD_ID            4
+#define ACK							false
+
+
+//#define MY_TRANSPORT_WAIT_READY_MS 5000
 //#include <SPI.h>
 #include <MySensors.h>
 #include <Bounce2.h>
 
 #define ONE_WIRE_BUS 12 // Pin where dallase sensor is connected 
 
-int RELAY_PIN[3] = { 5, 6, 11 }; // Arduino Digital I/O pin number for relay 
-int BUTTON_PIN[3] = { 7, 3, 4 };  // Arduino Digital I/O pin number for button 
-
 #define RELAY_ON 1
 #define RELAY_OFF 0
-int oldValue[3] = { HIGH, HIGH, HIGH };
-bool state[3];
-unsigned long lastTime;
+
 
 #define COMPARE_TEMP 1 // Send temperature only if changed? 1 = Yes 0 = No
-
 #define ONE_WIRE_BUS 12 // Pin where dallase sensor is connected 
 #define MAX_ATTACHED_DS18B20 1
 #define TEMPERATURE_PRECISION 10
+
+
+
+int oldValue[3] = { HIGH, HIGH, HIGH };
+bool state[3];
+unsigned long lastTime;
+bool connection_broken = false;
+
+int RELAY_PIN[3] = { 5, 6, 11 }; // Arduino Digital I/O pin number for relay 
+int BUTTON_PIN[3] = { 2, 3, 4 };  // Arduino Digital I/O pin number for button 
+
+bool first_message_sent = false;
 unsigned long SLEEP_TIME = 1000; // Sleep time between reads (in milliseconds)
-unsigned long iDsStop;
+
+
+AsyncTask dsRead (SLEEP_TIME, []() { ReadDs();   }); 
 OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 DallasTemperature sensors(&oneWire); // Pass the oneWire reference to Dallas Temperature. 
+
 float lastTemperature[MAX_ATTACHED_DS18B20];
 int numSensors = 0;
 bool receivedConfig = false;
@@ -80,56 +101,63 @@ Bounce debouncer[3]
 
 
 MyMessage msg[4] = {
-  MyMessage(0,V_LIGHT),
-  MyMessage(1,V_LIGHT),
-  MyMessage(2,V_LIGHT),
-  MyMessage(3,V_TEMP)
+  MyMessage(MS_RELAY1_CHILD_ID,V_LIGHT),
+  MyMessage(MS_RELAY2_CHILD_ID,V_LIGHT),
+  MyMessage(MS_RELAY3_CHILD_ID,V_LIGHT),
+  MyMessage(MS_TEMP_CHILD_ID,V_TEMP)
 };
-//void before()
-//{
-//	SetupDs182b();
-//
-//}
+
+
+
+void before()
+{
+	for (size_t i = 0; i < 3; i++)
+	{
+		// Setup the button
+		// Activate internal pull-up
+		pinMode(BUTTON_PIN[i], INPUT_PULLUP);
+		pinMode(RELAY_PIN[i], OUTPUT);
+
+		debouncer[i].attach(BUTTON_PIN[i]);
+		debouncer[i].interval(5);
+
+		state[i] = loadState(i);
+		// Make sure relays are off when starting up
+		digitalWrite(RELAY_PIN[i], RELAY_OFF);
+	}
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
 void setup()
 {
 	for (size_t i = 0; i < 3; i++)
 	{
-		state[i] = loadState(i);
-		// Setup the button
-		pinMode(BUTTON_PIN[i], INPUT_PULLUP);                               
-		// Activate internal pull-up
-		//digitalWrite(BUTTON_PIN[i], HIGH);                                                        
-
-		// After setting up the button, setup debouncer
-		debouncer[i].attach(BUTTON_PIN[i]);
-		debouncer[i].interval(5);
-
-
-		// Then set relay pins in output mode
-		pinMode(RELAY_PIN[i], OUTPUT);
-		// Make sure relays are off when starting up
-		digitalWrite(RELAY_PIN[i], RELAY_OFF);
-
 
 		// Set relay to last known state (using eeprom storage) 
+		//request(i + 1, V_LIGHT, NODE_ID);
 
 		digitalWrite(RELAY_PIN[i], state[i] ? HIGH : LOW);
+
 	}
 	// requestTemperatures() will not block current thread
 	SetupDs182b();
+	dsRead.Start();
+
+	BlinkLed(5,200);
+
 }
 
 void presentation() {
 	// Send the sketch version information to the gateway and Controller
-	sendSketchInfo("PatrykRooms", "1.0", false);
+	sendSketchInfo(MS_BOARD_NAME, MS_SOFTWARE_VERSION, ACK);
 
 	// Register all sensors to gw (they will be created as child devices)
-	for (size_t i = 0; i < 3; i++)
-	{
-		present(i, S_LIGHT);
-	}
-	present(3, S_TEMP);
-	
+
+	present(MS_RELAY1_CHILD_ID, S_LIGHT);
+	present(MS_RELAY2_CHILD_ID, S_LIGHT);
+	present(MS_RELAY3_CHILD_ID, S_LIGHT);
+	present(MS_TEMP_CHILD_ID, S_TEMP);
+
 }
 
 /*
@@ -137,6 +165,33 @@ void presentation() {
 */
 void loop()
 {
+
+	if (first_message_sent == false) {
+		Serial.println("Sending initial state...");
+		send_startup();
+		
+		first_message_sent = true;
+	}
+
+	//if (!isTransportReady())
+	//{
+	//	connection_broken = true;
+	//	BlinkLed(2, 500);
+	//}
+	//else if (connection_broken)
+	//{
+	//	for (size_t i = 0; i < 3; i++)
+	//	{
+
+	//		// Set relay to last known state (using eeprom storage) 
+	//		request(i + 1, V_LIGHT, NODE_ID);
+	//				
+	//	}
+	//}
+
+
+
+	dsRead.Update();
 	for (size_t i = 0; i < 3; i++)
 	{
 
@@ -146,51 +201,43 @@ void loop()
 		int value = debouncer[i].read();
 
 		if (value != oldValue[i]) {
-			/*Serial.println("*********************************************");
-			Serial.print("Button: ");
-			Serial.print(i);
-			Serial.print(" - Pin: ");
-			Serial.print(BUTTON_PIN[i]);
-			Serial.print("Stan: ");
-			Serial.println(value);
-			Serial.println("*********************************************");*/
-			//state[i] = !state[i];
-
-			send(msg[i].set(state[i] ? false : true), true);
+	
+			send(msg[i].set(state[i] ? false : true), ACK);
+			digitalWrite(RELAY_PIN[i], value);
 			oldValue[i] = value;
 		}
 
-		ReadDs(30000);
-		/*if (millis() - lastTime > SLEEP_TIME) {
+		
 
-
-			Serial.print(loadState(0));
-			Serial.print("-");
-			Serial.print(loadState(1));
-			Serial.print("-");
-			Serial.println(loadState(2));
-			lastTime = millis();
-		}*/
 	}
 
 }
+void send_startup() {
 
-void receive(const MyMessage &message) {
+	Serial.println("Sending initial state... ");
+
+	send(msg[0].set(state[0]), ACK);
+	send(msg[1].set(state[1]), ACK);
+	send(msg[2].set(state[2]), ACK);
+	ReadDs();
+}
+void receive(const MyMessage& message) {
 	// We only expect one type of message from controller. But we better check anyway.
 	if (message.isAck()) {
 		Serial.println("This is an ack from gateway");
 	}
 
 	if (message.type == V_LIGHT) {
+		int i = message.sensor - 1;
 		// Change relay state
-		state[message.sensor] = message.getBool();
+		state[i] = message.getBool();
 		/*Serial.print("Pin: ");
 		Serial.print(RELAY_PIN[message.sensor]);
 		Serial.print(", New status: ");
 		Serial.println(state[message.sensor] ? HIGH : LOW);*/
-		digitalWrite(RELAY_PIN[message.sensor], state[message.sensor] ? HIGH : LOW);
+		digitalWrite(RELAY_PIN[i], state[i] ? HIGH : LOW);
 		// Store state in eeprom
-		saveState(message.sensor, state[message.sensor]);
+		saveState(i, state[i]);
 
 		//// Write some debug info
 		/*Serial.print("Incoming change for sensor:");
@@ -217,41 +264,49 @@ void SetupDs182b() {
 	}
 
 }
-void ReadDs(unsigned long interval)
+void ReadDs()
 {
-	if (millis() - iDsStop >= interval && numSensors > 0)
-	{
-		// Fetch temperatures from Dallas sensors
-		sensors.requestTemperatures();
+	// Fetch temperatures from Dallas sensors
+	sensors.requestTemperatures();
 
-		// query conversion time and sleep until conversion completed
-		int16_t conversionTime = sensors.millisToWaitForConversion(sensors.getResolution());
-		// sleep() call can be replaced by wait() call if node need to process incoming messages (or if node is repeater)
-		sleep(conversionTime);
+	// query conversion time and sleep until conversion completed
+	int16_t conversionTime = sensors.millisToWaitForConversion(sensors.getResolution());
+	// sleep() call can be replaced by wait() call if node need to process incoming messages (or if node is repeater)
+	sleep(conversionTime);
 
-		// Read temperatures and send them to controller 
-		for (int i = 0; i < numSensors && i < MAX_ATTACHED_DS18B20; i++) {
+	// Read temperatures and send them to controller 
+	for (int i = 0; i < numSensors && i < MAX_ATTACHED_DS18B20; i++) {
 
-			// Fetch and round temperature to one decimal
-			float temperature = static_cast<float>(static_cast<int>((getControllerConfig().isMetric ? sensors.getTempCByIndex(i) : sensors.getTempFByIndex(i)) * 10.)) / 10.;
-			Serial.print("Temperature: ");
-			Serial.println(temperature);
-			// Only send data if temperature has changed and no error
+		// Fetch and round temperature to one decimal
+		float temperature = static_cast<float>(static_cast<int>((getControllerConfig().isMetric ? sensors.getTempCByIndex(i) : sensors.getTempFByIndex(i)) * 10.)) / 10.;
+		Serial.print("Temperature: ");
+		Serial.println(temperature);
+		// Only send data if temperature has changed and no error
 #if COMPARE_TEMP == 1
-			if (lastTemperature[i] != temperature && temperature != -127.00 && temperature != 85.00) {
+		if (lastTemperature[i] != temperature && temperature != -127.00 && temperature != 85.00) {
 #else
-			if (temperature != -127.00 && temperature != 85.00) {
+		if (temperature != -127.00 && temperature != 85.00) {
 #endif
 
-				// Send in the new temperature
-				send(msg[3].set(temperature, 1));
-				// Save new temperatures for next compare
-				lastTemperature[i] = temperature;
-			}
-
+			// Send in the new temperature
+			send(msg[3].set(temperature, 1));
+			// Save new temperatures for next compare
+			lastTemperature[i] = temperature;
 		}
-		iDsStop = millis();
-	}
-	
 
+	}
+
+
+}
+
+void BlinkLed(int count, uint32_t time )
+{
+	digitalWrite(LED_BUILTIN, LOW);
+	for (int i = 0; i < count; i++)
+	{
+		digitalWrite(LED_BUILTIN, HIGH);
+		sleep(time);
+		digitalWrite(LED_BUILTIN, LOW);
+		sleep(time / 2);
+	}
 }
